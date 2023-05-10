9028b68692bccea863ae66d756b14adebb54b03f
sxmbaka <yatharth.singhhh@gmail.com>
2023-05-10 14:14:12 +0530

    update 1 file, copy 2 files, create 1 file and move 2 files

---
+++ codechef/Number_Mirror.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define fr (x) for (int i = 0; i < x; i++)
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << n;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
