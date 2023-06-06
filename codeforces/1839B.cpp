f1d1d00feb0da0719d55f3d3dca18b29100f8ea4
sxmbaka <yatharth.singhhh@gmail.com>
2023-06-06 14:16:51 +0530

    feat: create 1839B.cpp

---
+++ codeforces/1839B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define ll long long
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define fr (x) for (int i = 0; i < x; i++)
+
+void solve()
+{
+    int n;
+    int n;
+    cin >> n;
+    vector<int> a(n), b(i);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+        cin >> b[i];
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll testcase = 0;
+    cin >> testcase;
+    while (testcase--)
+    {
+        solve();
+    }
+    return 0;
+}
